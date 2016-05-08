30a0397bb51a9734bcde342cdafb3ddbdefea731
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-05-08 13:43:47 +0200

    Fixes

--- check.py
+++ check.py
@@ -8,7 +8,7 @@ import time
 config = __import__("user-config")
 
 
-def check_filename(page):
+def check_filename(page,simul):
     '''This function make the first letter of
     filenames uppercase'''
     newtext = page.text[:]
@@ -23,11 +23,11 @@ def check_filename(page):
             f = f[0].upper()+f[1:]
             newtext = newtext.replace(match.group(0),'[[File:'+f+']]')
             changed = True
-    if changed:
+    if changed and not simul:
         page.text = newtext
         page.save(minor=True, botflag=True)
 
-def move_page(site,page, title):
+def move_page(site,page, title,simul):
     letter = title[5]
     if letter.islower():
         global errors
@@ -38,16 +38,18 @@ def move_page(site,page, title):
         if site.page_exists(new_page):
             #deleting the page
             print("        #Duplicate image: "+ title)
+            global images_dupl
+            images_dupl.append(title)
             #I'm not able to delete them
             #page.delete(prompt=False, reason='duplicated file', mark=True)
         else:
             print("        #Moving image: "+ title)
             try:
-                page.move(new_title,reason='Wrong filename capitalization')
+                if not simul:
+                    page.move(new_title,
+                        reason='Wrong filename capitalization')
             except Exception as e:
                 print('ERROR: '+str(e))
-
-
 
 
 def main(MODE):
@@ -61,6 +63,8 @@ def main(MODE):
 
     checkedPages = 0
     global errors
+    global images_dupl
+    images_dupl =[]
     errors = 0
 
     if MODE == 'a':
@@ -69,15 +73,31 @@ def main(MODE):
             page_title = page.title()
             print("    @page: " + page_title + "")
             page = pywikibot.Page(site, page_title)
-            check_filename(page)
+            check_filename(page,False)
             checkedPages+=1
     elif MODE == 'b':
         print("Checking all images")
         for page in site.allimages(step=20):
             page_title = page.title()
             print("    @image: " + page_title + "")
-            move_page(site,page, page_title)
+            move_page(site,page, page_title,False)
+            checkedPages+=1
+    elif MODE == 'as':
+        print("Checking all pages SIMULATED")
+        for page in site.allpages(step=20):
+            page_title = page.title()
+            print("    @page: " + page_title + "")
+            page = pywikibot.Page(site, page_title)
+            check_filename(page,True)
+            checkedPages+=1
+    elif MODE == 'bs':
+        print("Checking all images SIMULATED")
+        for page in site.allimages(step=20):
+            page_title = page.title()
+            print("    @image: " + page_title + "")
+            move_page(site,page, page_title,True)
             checkedPages+=1
 
     print("Checked " + str(checkedPages) + " pages")
     print("Fixed "+ str(errors) + " pages")
+    print("Duplicated images:\n"+ '\n'.join(images_dupl))
